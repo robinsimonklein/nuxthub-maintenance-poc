ea4f090358d58f416e1d67e1b933d1fedd3fb887
Robin Simonklein <rsimonklein@gmail.com>
2024-09-04 20:31:36 +0200

    feat: Add _maintenance route

---
+++ server/routes/_maintenance.ts
@@ -0,0 +1,18 @@
+export default defineEventHandler(async event => {
+    return ` <!DOCTYPE html>
+        <html lang="en" style="">
+          <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>Under Maintenance</title>
+            <meta name="MSSmartTagsPreventParsing" content="true">
+            <meta http-equiv="imagetoolbar" content="false">
+            <meta name="robots" content="nocache">
+            <meta name="robots" content="noindex,nofollow">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body style="height: 100vh; width: 100vw; display: flex; justify-content: center; align-items: center">
+          <h1>Under Maintenance</h1>
+        </body>
+      </html>
+    `
+})
